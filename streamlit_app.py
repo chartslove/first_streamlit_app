964821df498dc62376e047830f5d2826c1e6b605
chartslove <129827743+chartslove@users.noreply.github.com>
2023-04-04 12:14:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,4 +12,5 @@ streamlit.text('🐔 🥑 🍞 Hard-Boiled Free-Range Egg')
 
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
