c9f50acf5a9832c03c5628d20d89168ba00272fb
chartslove <129827743+chartslove@users.noreply.github.com>
2023-04-09 23:50:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ try:
     fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
     if not fruit_choice:
       streamlit.error("Please select fruit")
-    else
+    else:
       streamlit.write('The user entered ', fruit_choice)
       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
       # Get data in table normalized form 
